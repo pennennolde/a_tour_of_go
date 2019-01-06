2ca7220f68f9bb1bd918a4a03af12683351238c2
pennennolde <pennennolde@macbook-pro-2017.local>
2019-01-06 12:15:44 +0900

    add basics-23_if_and_else

---
+++ basics-23_if_and_else/basics-23_if_and_else.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		// ifステートメントで宣言された変数は、elseブロック内でも使うことができる
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
+
